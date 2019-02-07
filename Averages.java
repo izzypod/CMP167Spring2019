b13a73d93ad73213137518b061435bc9d2e90061
izzypod <47427994+izzypod@users.noreply.github.com>
2019-02-07 12:28:58 -0500

    Add files via upload

---
+++ Averages.java
@@ -0,0 +1,30 @@
+/*
+ * author: isabel podolsky
+ * created: 2-7-19
+ * description: this program will find the average of three numbers.
+ */
+package week2;
+import java.util.Scanner;
+public class Averages {
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		
+		double firstNumber;
+		double secondNumber;
+		double thirdNumber;
+		double avgNumber;
+		
+		System.out.println("Enter first number: ");
+		firstNumber = scnr.nextDouble();
+		System.out.println("Enter second number: ");
+		secondNumber = scnr.nextDouble();
+		System.out.println("Enter third number: ");
+		thirdNumber = scnr.nextDouble();
+		
+		avgNumber = (firstNumber + secondNumber + thirdNumber) / 3.0;
+		System.out.println("The average is: " + avgNumber);
+		
+		
+	}
+
+}

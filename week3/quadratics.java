93170fa4d0e808516c3b68f45fa293e83477d849
izzypod <47427994+izzypod@users.noreply.github.com>
2019-02-14 12:05:12 -0500

    Update quadratics.java

--- week3/quadratics.java
+++ week3/quadratics.java
@@ -12,19 +12,23 @@ import java.util.Scanner;
 public class quadratics {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
+	   
       double avar;
       double bvar;
       double cvar;
+	   
       System.out.println("a:");
       avar = scnr.nextDouble();
       System.out.println("b:");
       bvar = scnr.nextDouble();
       System.out.println("c:");
       cvar = scnr.nextDouble();
+	   
       double discriminant = Math.pow(bvar, 2) - (4 * avar * cvar);
       double discriminantrt = Math.sqrt(discriminant);
       double root1 = ((-1 * bvar) + discriminantrt) / (avar * 2.0);
       double root2 = ((-1 * bvar) - discriminantrt) / (avar * 2.0);
+	   
       System.out.println("Your first root is: " + root1);
       System.out.println("Your second root is: " + root2);
       

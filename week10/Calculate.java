8cf18ff35e20fb092bdc51f6f4e97f4edfffe184
izzypod <47427994+izzypod@users.noreply.github.com>
2019-04-02 12:16:26 -0400

    Create Calculate.java

---
+++ week10/Calculate.java
@@ -0,0 +1,15 @@
+public class Calculate {
+	
+	public static int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public static double multiply(double a, double b) {
+		return a * b;
+	}
+	
+	public static double getRectangleArea(double width, double length) {
+		return width * length;
+	}
+	
+}
